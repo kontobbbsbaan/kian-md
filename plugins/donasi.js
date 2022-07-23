2400edb2c95d5ec4d22bf2df66985fdcbfcb2616
kontobbbsbaan <108917081+kontobbbsbaan@users.noreply.github.com>
2022-07-23 12:57:22 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
-┏──「 Donasi • Dana 」─⬣
+┏──「 Donasi 」─⬣
 │ • Pulsa : 085850539404
-│ • Dana : 085850539404
-╰────────⬣
+
+
 ┏──「 *NOTE* 」─⬣
 │ > Ingin donasi? wa.me/6285850539404
 │ _Hasil donasi akan digunakan buat sewa_
