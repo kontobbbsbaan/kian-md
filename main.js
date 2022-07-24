6fe59a8655f970527e02cbb35f3ba00f8b668bc7
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-07-24 07:57:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ async function connectionUpdate(update) {
   if (connection == 'connecting') console.log(chalk.redBright('🕛 Mengaktifkan Bot, Harap tunggu sebentar...'))
   if (connection == 'open') {
       console.log(chalk.green('Connected✅'))
-      await conn.hehe("6281320170984@s.whatsapp.net", global.ftoli)
+      await conn.hehe("6283845409710@s.whatsapp.net", global.ftoli)
   }
   if (connection == 'close') console.log(chalk.red('⏹️Koneksi berhenti dan mencoba menghubungkan kembali...'))
   global.timestamp.connect = new Date
