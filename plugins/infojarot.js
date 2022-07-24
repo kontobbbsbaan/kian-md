a7352102f052df66a7e526d5c69dab460dc4f5a5
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-07-24 07:37:56 +0700

    Update infojarot.js

--- plugins/infojarot.js
+++ plugins/infojarot.js
@@ -1,21 +1,13 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : Jarot
-│ ✎ _Sekolah_ : Lulus
-│ ✎ _Umur_ : 17
+│ ✎ _Nama_ : kian
+│ ✎ _Sekolah_ : Smk
+│ ✎ _Umur_ : 15
 │ ✎ _Asal_ : Jawa timur
 │ ✎ _Status_ : Turu deck
-│ ✎ _Official Grup 1_ :
-│    https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL
-│ ✎ _Official Grup 2_ :
-│    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
-│ ✎ _Youtube _:
-│    https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
-│ ✎ _Instagram_ : 
-│    https://instagram.com/oscarbotz_
 │ ✎ _WhatsApp_ :
-│    wa.me/6285850539404
+│    wa.me/6283845409710
 ╰───────────────────
 `.trim(), m)
 }
